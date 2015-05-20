8cafc6ffe2c7d09d0456b9ab6019f82d1a663402
vkolova <onlysilence@abv.bg>
2015-05-20 20:42:32 +0300

    Video Upload

--- js/upload_video.js
+++ js/upload_video.js
@@ -18,11 +18,151 @@ function loadUpload() {
 }
 
 function uploadVideo() {
-	alert('access_token success' + accessToken);
-
 	var uploadVideo = new UploadVideo();
-	uploadVideo.ready(accessToken);
-
-	
-	
+	var res = uploadVideo.ready(accessToken);
 }
+
+var STATUS_POLLING_INTERVAL_MILLIS = 60 * 1000; // One minute.
+
+var UploadVideo = function() {
+
+  this.tags = ['youtube-cors-upload'];
+
+  this.categoryId = 22;
+
+  this.videoId = '';
+
+  this.uploadStartTime = 0;
+};
+
+
+UploadVideo.prototype.ready = function(accessToken) {
+  this.accessToken = accessToken;
+  this.gapi = gapi;
+  this.authenticated = true;
+  this.gapi.client.request({
+    path: '/youtube/v3/channels',
+    params: {
+      part: 'snippet',
+      mine: true
+    },
+    callback: function(response) {
+      if (response.error) {
+        console.log(response.error.message);
+      } else {
+        $('#channel-name').text(response.items[0].snippet.title);
+        $('#channel-thumbnail').attr('src', response.items[0].snippet.thumbnails.default.url);
+
+        $('.pre-sign-in').hide();
+        $('.post-sign-in').show();
+      }
+    }.bind(this)
+  });
+  $('#button').on("click", this.handleUploadClicked.bind(this));
+};
+
+
+UploadVideo.prototype.uploadFile = function(file) {
+  var metadata = {
+    snippet: {
+      title: $('#title').val(),
+      description: $('#description').text(),
+      tags: this.tags,
+      categoryId: this.categoryId
+    },
+    status: {
+      privacyStatus: $('#privacy-status option:selected').text()
+    }
+  };
+  var uploader = new MediaUploader({
+    baseUrl: 'https://www.googleapis.com/upload/youtube/v3/videos',
+    file: file,
+    token: this.accessToken,
+    metadata: metadata,
+    params: {
+      part: Object.keys(metadata).join(',')
+    },
+    onError: function(data) {
+      var message = data;
+      // Assuming the error is raised by the YouTube API, data will be
+      // a JSON string with error.message set. That may not be the
+      // only time onError will be raised, though.
+      try {
+        var errorResponse = JSON.parse(data);
+        message = errorResponse.error.message;
+      } finally {
+        alert(message);
+      }
+    }.bind(this),
+    onProgress: function(data) {
+      var currentTime = Date.now();
+      var bytesUploaded = data.loaded;
+      var totalBytes = data.total;
+      // The times are in millis, so we need to divide by 1000 to get seconds.
+      var bytesPerSecond = bytesUploaded / ((currentTime - this.uploadStartTime) / 1000);
+      var estimatedSecondsRemaining = (totalBytes - bytesUploaded) / bytesPerSecond;
+      var percentageComplete = (bytesUploaded * 100) / totalBytes;
+
+      $('#upload-progress').attr({
+        value: bytesUploaded,
+        max: totalBytes
+      });
+
+      $('#percent-transferred').text(percentageComplete);
+      $('#bytes-transferred').text(bytesUploaded);
+      $('#total-bytes').text(totalBytes);
+
+      $('.during-upload').show();
+    }.bind(this),
+    onComplete: function(data) {
+      var uploadResponse = JSON.parse(data);
+      this.videoId = uploadResponse.id;
+      $('#video-id').text(this.videoId);
+      $('.post-upload').show();
+      this.pollForVideoStatus();
+    }.bind(this)
+  });
+  // This won't correspond to the *exact* start of the upload, but it should be close enough.
+  this.uploadStartTime = Date.now();
+  uploader.upload();
+};
+
+UploadVideo.prototype.handleUploadClicked = function() {
+  $('#button').attr('disabled', true);
+  this.uploadFile($('#file').get(0).files[0]);
+};
+
+UploadVideo.prototype.pollForVideoStatus = function() {
+  this.gapi.client.request({
+    path: '/youtube/v3/videos',
+    params: {
+      part: 'status,player',
+      id: this.videoId
+    },
+    callback: function(response) {
+      if (response.error) {
+        // The status polling failed.
+        console.log(response.error.message);
+        setTimeout(this.pollForVideoStatus.bind(this), STATUS_POLLING_INTERVAL_MILLIS);
+      } else {
+        var uploadStatus = response.items[0].status.uploadStatus;
+        switch (uploadStatus) {
+          // This is a non-final status, so we need to poll again.
+          case 'uploaded':
+            $('#post-upload-status').append('<li>Upload status: ' + uploadStatus + '</li>');
+            setTimeout(this.pollForVideoStatus.bind(this), STATUS_POLLING_INTERVAL_MILLIS);
+            break;
+          // The video was successfully transcoded and is available.
+          case 'processed':
+            $('#player').append(response.items[0].player.embedHtml);
+            $('#post-upload-status').append('<li>Final status.</li>');
+            break;
+          // All other statuses indicate a permanent transcoding failure.
+          default:
+            $('#post-upload-status').append('<li>Transcoding failed.</li>');
+            break;
+        }
+      }
+    }.bind(this)
+  });
+};
