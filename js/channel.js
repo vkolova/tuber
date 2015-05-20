cb5f2e5f0503b3f473dd5590b9cde2089e015674
Valentin Georgiev <valio.v.georgiev@gmail.com>
2015-05-20 11:34:33 +0300

    Index Construction

--- js/channel.js
+++ js/channel.js
@@ -26,5 +26,54 @@ function displayChannel(response) {
 		playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
 		requestVideoPlaylist(playlistId);
 	}
+}
 
+function requestVideoPlaylist(playlistId, pageToken) {
+//	document.getElementById('app-body').innerHTML = '';
+//	document.getElementById('app-body').innerHTML += '<div id="video-container"></div>';
+	$('#video-container').html('');
+	
+	var requestOptions = {
+		playlistId: playlistId,
+		part: 'snippet',
+		maxResults: 10
+	};
+	if (pageToken) {
+		requestOptions.pageToken = pageToken;
+	}
+	var request = gapi.client.youtube.playlistItems.list(requestOptions);
+	request.execute(function(response) {
+	// Only show pagination buttons if there is a pagination token for the
+	// next or previous page of results.
+	
+		nextPageToken = response.result.nextPageToken;
+		var nextDis = nextPageToken ? '' : 'disabled';
+		$('.next').css('class', "next" + nextDis);
+		
+		prevPageToken = response.result.prevPageToken
+		var prevDis = prevPageToken ? '' : 'disabled';
+		$('.previous').css('class', "previous" + prevDis);
+
+		var playlistItems = response.result.items;
+		if (playlistItems) {
+			$.each(playlistItems, function(index, item) {
+				displayResult(item.snippet);
+			});
+		} else {
+		$('#video-container').html('<div class="alert alert-info" role="alert">Sorry, you have no uploaded videos :(</div>');
+		}
+	});
+}
+
+function displayResult(videoSnippet) {
+	var title = videoSnippet.title;
+	var videoId = videoSnippet.resourceId.videoId;
+}
+
+function nextPage() {
+	requestVideoPlaylist(playlistId, nextPageToken);
+}
+
+function previousPage() {
+	requestVideoPlaylist(playlistId, prevPageToken);
 }
